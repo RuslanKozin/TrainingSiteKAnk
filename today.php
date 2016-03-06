fa467f2a81fccd4afc1fba6b4260bf99992067e4
RuslanKozin <ruslan3723@mail.ru>
2016-03-06 20:02:40 +0300

    Create today.php

---
+++ today.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Сегодняшняя дата</title>
+</head>
+<body>
+    <p>Сегодняшняя дата (согласно данному веб-серверу):
+        <?php
+            echo date('l, F jS Y.');
+        ?>
+    </p>
+</body>
+</html>
