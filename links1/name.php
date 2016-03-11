d8887e17962754f48ba503555d14cfabf1c0b208
RuslanKozin <ruslan3723@mail.ru>
2016-03-11 16:41:34 +0300

    Добавил функцию htmlspecialchars и комментарий

--- links1/name.php
+++ links1/name.php
@@ -7,7 +7,16 @@
 <body>
     <?php
         $name = $_GET['name'];
-        echo 'Добро пожаловать на наш веб-сайт, ' . $name . '!';
+        echo 'Добро пожаловать на наш веб-сайт, ' .
+            htmlspecialchars($name, ENT_QUOTES, 'UTF-8') . '!';
+
+    /*    htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
+        htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
+     htmlspecialchars - преобразует символы < и > в &lt; и &gt; и браузер не может интерпретировать html как код.
+       ENT_QUOTES - сообщает функции htmlspecialchars, что кроме прочих спец. символов, нужно преобразовывать еще одинарные и двойные кавычки.
+       UTF-8 - какую кодировку нужно использовать для интерпретации заданного текста.
+    */
+
     ?>
 </body>
 </html>
