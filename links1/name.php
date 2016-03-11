6c682bc095a58764f16dfb05794cd14b767cc0cc
RuslanKozin <ruslan3723@mail.ru>
2016-03-11 18:56:45 +0300

    Изменил переменные

--- links1/name.php
+++ links1/name.php
@@ -6,9 +6,11 @@
 </head>
 <body>
     <?php
-        $name = $_GET['name'];
+        $firstname = $_GET['firstname'];
+        $lastname = $_GET['lastname'];
         echo 'Добро пожаловать на наш веб-сайт, ' .
-            htmlspecialchars($name, ENT_QUOTES, 'UTF-8') . '!';
+            htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . '' .
+            htmlspecialchars($lastname, ENT_QUOTES, 'UTF-8') . '!';
 
     /*    htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
         htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
