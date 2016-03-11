4d7b1488b8c8831c5bfd8648e6d2b908f2153fd8
RuslanKozin <ruslan3723@mail.ru>
2016-03-11 19:07:51 +0300

    Добавил пробел

--- links1/name.php
+++ links1/name.php
@@ -9,7 +9,7 @@
         $firstname = $_GET['firstname'];
         $lastname = $_GET['lastname'];
         echo 'Добро пожаловать на наш веб-сайт, ' .
-            htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . '' .
+            htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . ' ' .
             htmlspecialchars($lastname, ENT_QUOTES, 'UTF-8') . '!';
 
     /*    htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
