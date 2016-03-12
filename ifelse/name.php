1a1f6891693fa9c69e692eb3ad935646fde39dc4
RuslanKozin <ruslan3723@mail.ru>
2016-03-12 17:21:06 +0300

    Добавил код php

--- ifelse/name.php
+++ ifelse/name.php
@@ -5,6 +5,17 @@
     <title>Document</title>
 </head>
 <body>
-
+    <?php
+        $firstname = $_REQUEST['firstname'];
+        $lastname = $_REQUEST['lastname'];
+        if($firstname == 'Кевин' and $lastname == 'Янк'){
+            echo 'Добро пожаловать, о блистательный правитель!';
+        }
+        else{
+            echo 'Добро пожаловать на наш сайт веб-сайт, ' .
+                htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . ' ' .
+                htmlspecialchars($lastname, ENT_QUOTES, 'UTF-8') . '!';
+        }
+    ?>
 </body>
 </html>
