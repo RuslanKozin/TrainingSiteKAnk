abf6cd6e869a39c7d0cf7223b808829792db619d
RuslanKozin <ruslan3723@mail.ru>
2016-03-12 16:43:03 +0300

    Создал файл name.php и добавил условие

---
+++ if/name.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $name = $_REQUEST['name'];
+        if($name == 'Кевин'){
+            echo 'Добро пожаловать, о блистательный правитель!';
+        }
+    ?>
+</body>
+</html>
