3d534ea41311317557793d9b35cb96165c6693c8
RuslanKozin <ruslan3723@mail.ru>
2016-03-14 19:01:11 +0300

    Создал папку welcome и файл form.html.php

---
+++ welcome/form.html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <form action="" method="post">
+        <div>
+            <label for="firstname">Имя:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="text" name="firstname" id="firstname">
+            </label>
+        </div>
+        <div>
+            <label for="lastname">Фамилия:
+                <input type="text" name="lastname" id="lastname">
+            </label>
+        </div>
+        <div><input type="submit" value="Поехали"></div>
+    </form>
+</body>
+</html>
