5245265cf4ef2bf3f3ed3fe1ab50f4f4a01628c7
RuslanKozin <ruslan3723@mail.ru>
2016-03-16 18:58:19 +0300

    Добавил код

--- listjokes/index.php
+++ listjokes/index.php
@@ -1,1 +1,10 @@
 <?php
+try{
+    $sql = 'SELECT joketext FROM joke';
+    $result = $pdo->query($sql);
+}
+catch(PDOException $e){
+    $error = 'Ошибка при извлечении шуток: ' . $e->getMessage();
+    include 'error.html.php';
+    exit();
+}
