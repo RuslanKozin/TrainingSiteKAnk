367654e78003c29b5c384a09b9cea490aa6e90a1
RuslanKozin <ruslan3723@mail.ru>
2016-03-16 15:42:38 +0300

    Добавил код

--- updatechicken/index.php
+++ updatechicken/index.php
@@ -1,1 +1,12 @@
 <?php
+try{
+    $sql = 'UPDATE joke SET jokedate="2012-04-01" WHERE joketext LIKE "%цыпленок%"';
+    $affectedRows = $pdo->exec($sql);
+}
+catch(PDOException $e){
+    $output = 'Ошибка при выполнении обновления: ' . $e->getMessage();
+    include 'output.html.php';
+    exit();
+}
+$output = "Обновлено столбцов: $affectedRows";
+include 'output.html.php';
