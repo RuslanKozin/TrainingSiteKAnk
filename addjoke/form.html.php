32d91a6e1f851125f180a6adef619d6535f89ee9
RuslanKozin <ruslan3723@mail.ru>
2016-03-21 18:25:40 +0300

    Добавил код в файл form.html.php, добавил файл jokes.html.php в папку и добавил код

--- addjoke/form.html.php
+++ addjoke/form.html.php
@@ -1,1 +1,22 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Добавление шутки</title>
+    <style type="text/css">
+        textarea{
+            display: block;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form action="?" method="post">
+        <div>
+            <label for="joketext">Введите сюда свою шутку:</label>
+            <textarea name="joketext" id="joketext" cols="40" rows="3"></textarea>
+        </div>
+        <div><input type="submit" value="Добавить"></div>
+    </form>
+</body>
+</html>
