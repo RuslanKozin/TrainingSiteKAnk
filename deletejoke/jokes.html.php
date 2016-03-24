46c8b18a67d06a49db98ccbb4c36ffa5244ce4ea
RuslanKozin <ruslan3723@mail.ru>
2016-03-24 14:15:36 +0300

    Добавил код

--- deletejoke/jokes.html.php
+++ deletejoke/jokes.html.php
@@ -8,11 +8,15 @@
     <p><a href="?addjoke">Добавьте собственную шутку</a></p>
     <p>Вот все шутки, которые есть в базе данных:</p>
     <?php foreach ($jokes as $joke): ?>
-        <blockquote>
-            <p>
-                <?php echo htmlspecialchars($joke, ENT_QUOTES, 'UTF-8');?>
-            </p>
-        </blockquote>
+        <form action="?deletejoke" method="post">
+            <blockquote>
+                <p>
+                    <?php echo htmlspecialchars($joke['text'], ENT_QUOTES, 'UTF-8');?>
+                    <input type="hidden" name="id" value="<?php echo $joke['id'];?>">
+                    <input type="submit" value="Удалить">
+                </p>
+            </blockquote>
+        </form>
     <?php endforeach; ?>
 </body>
 </html>
