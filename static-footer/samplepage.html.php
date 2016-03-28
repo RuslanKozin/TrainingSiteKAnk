39c5ed5fd9b25faa15c657e8c681acf711531561
RuslanKozin <ruslan3723@mail.ru>
2016-03-28 14:41:40 +0300

    Добавил код

---
+++ static-footer/samplepage.html.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <p id="main">
+        Эта страница использует статические включения для вывода внизу стандартного предупреждения об авторских правах.
+    </p>
+    <?php include 'footer.inc.html.php';?>
+</body>
+</html>
