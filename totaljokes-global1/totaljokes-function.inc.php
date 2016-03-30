137c1cab2fd45404d334dfcec1972c1100e157dc
RuslanKozin <ruslan3723@mail.ru>
2016-03-30 23:15:19 +0300

    Добавил код

--- totaljokes-global1/totaljokes-function.inc.php
+++ totaljokes-global1/totaljokes-function.inc.php
@@ -1,1 +1,15 @@
 <?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
+    function totalJokes() {
+        global  $pdo;
+    try {
+        $result = $pdo->query('SELECT COUNT(*) FROM joke');
+    }
+    catch (PDOException $e) {
+        $error = 'Ошибка базы данных при подсчете шуток!';
+        include 'error.html.php';
+        exit();
+    }
+        $row = $result->fetch();
+        return $row[0];
+    }
