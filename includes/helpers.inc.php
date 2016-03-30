ed96f09580c5787d3c8b4fdf34c52e4ea317f5d4
RuslanKozin <ruslan3723@mail.ru>
2016-03-31 01:17:30 +0300

    Добавил комментарии

--- includes/helpers.inc.php
+++ includes/helpers.inc.php
@@ -1,8 +1,10 @@
 <?php
             /*Вспомогательные функции*/
+    /*Функция для вывода списка шуток, добавленных посетителями*/
 function html($text) {
     return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
 }
-function htmlout($text) {  /*htmlout принимает значение от функции html и выводит его на страницу*/
+    /*Функция htmlout принимает значение от функции html и выводит его на страницу*/
+function htmlout($text) {
     echo html($text);
 }
