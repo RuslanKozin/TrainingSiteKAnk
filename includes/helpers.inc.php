2b98613a7bf9c5d7fa78b18be3587b4eee4666d5
RuslanKozin <ruslan3723@mail.ru>
2016-03-30 23:54:55 +0300

    Добавил код и комментарии

--- includes/helpers.inc.php
+++ includes/helpers.inc.php
@@ -1,1 +1,8 @@
 <?php
+            /*Вспомогательные функции*/
+function html($text) {
+    return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
+}
+function htmlout($text) {  /*htmlout принимает значение от функции html и выводит его на страницу*/
+    echo html($text);
+}
