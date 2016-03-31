e98fc8897085061e2302c485962040ee39265f25
RuslanKozin <ruslan3723@mail.ru>
2016-03-31 18:58:34 +0300

    Добавил код

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,15 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Шуточная CMS</title>
+</head>
+<body>
+    <h1>Система управления шутками</h1>
+    <ul>
+        <li><a href="jokes/">Управление шутками</a></li>
+        <li><a href="authors/">Управление авторами</a></li>
+        <li><a href="categories/">Управление категориями шуток</a></li>
+    </ul>
+</body>
+</html>
