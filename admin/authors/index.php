4ad9d2367bdda632898ec3a8e9c16161b5d95fec
RuslanKozin <ruslan3723@mail.ru>
2016-04-02 15:14:53 +0300

    Добавил код

--- admin/authors/index.php
+++ admin/authors/index.php
@@ -1,1 +1,16 @@
 <?php
+//Выводим список авторов
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php'; //подключаем файл соединение с базой данных
+
+try {
+    $result = $pdo->query('SELECT id, name FROM author');
+}
+catch (PDOException $e) {
+    $error = 'Ошибка при извлечении авторов из базы данных!' . $e->getMessage();
+    include 'error.html.php';
+    exit();
+}
+foreach ($result as $row) {
+    $authors[] = array('id' => $row['id'], 'name' => $row['name']);
+}
+include 'authors.html.php';
