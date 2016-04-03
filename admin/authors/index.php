2a058457ded13eeca22c4aef616971dace59b6f6
RuslanKozin <ruslan3723@mail.ru>
2016-04-03 19:14:46 +0300

    Добавил код удаление автора(категорий, шуток, имен авторов)

--- admin/authors/index.php
+++ admin/authors/index.php
@@ -1,4 +1,71 @@
 <?php
+/* ..................... Удаление автора ......................... */
+if(isset($_POST['action']) and $_POST['action'] == 'Удалить') {
+    include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
+
+    //Получаем шутки, принадлежащие автору.
+    try {
+        $sql = 'SELECT id FROM joke WHERE authorid = :id';
+        $s = $pdo->prepare($sql);
+        $s->bindValue(':id', $_POST['id']);
+        $s->execute();
+    }
+    catch (PDOException $e) {
+        $error = 'Ошибка при получении списка шуток, которые нужно удалить.' . $e->getMessage();
+        include 'error.html.php';
+        exit();
+    }
+
+    $result = $s->fetchAll();
+
+    //Удаление записи о категориях шуток.
+    try {
+        $sql = 'DELETE FROM jokecategory WHERE jokeid = :id';
+        $s = $pdo->prepare($sql);
+
+        //Для каждой шутки.
+        foreach ($result as $row) {
+            $jokeId = $row['Id'];
+            $s->bindValue(':id', $jokeId);
+            $s->execute();
+        }
+    }
+    catch (PDOException $e) {
+        $error = 'Ошибка при удалении записей о категориях шутки.';
+        include 'error.html.php';
+        exit();
+    }
+
+    //Удалеляем шутки, принадлежащие автору.
+    try {
+        $sql = 'DELETE FROM joke WHERE authorid = :id';
+        $s = $pdo->prepare($sql);
+        $s->bindValue(':id', $_POST['id']);
+        $s->execute();
+    }
+    catch (PDOException $e) {
+        $error = 'Ошибка при удалении шуток, принадлежащих автору.';
+        include 'error.html.php';
+        exit();
+    }
+
+    //Удаляем имя автора.
+    try {
+        $sql = 'DELETE FROM author WHERE id = :id';
+        $s = $pdo->prepare($sql);
+        $s->bindValue(':id', $_POST['id']);
+        $s->execute();
+    }
+    catch (PDOException $e) {
+        $error = 'Ошибка при удалении автора.';
+        include 'error.html.php';
+        exit();
+    }
+    header('Location: .');
+    exit();
+}
+/*................................................................*/
+
 //Выводим список авторов
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php'; //подключаем файл соединение с базой данных
 
