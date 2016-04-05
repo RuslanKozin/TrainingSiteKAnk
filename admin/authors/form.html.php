eac8cdc87d8b02d5b24beda77aae79632436c24d
RuslanKozin <ruslan3723@mail.ru>
2016-04-05 20:11:56 +0300

    Добавил код

--- admin/authors/form.html.php
+++ admin/authors/form.html.php
@@ -1,1 +1,23 @@
-<?php
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/helpers.inc.php';?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php htmlout($pageTitle);?></title>
+</head>
+<body>
+    <h1><?php htmlout($pageTitle);?></h1>
+    <form action="?<?php htmlout($action);?>" method="post">
+        <div>
+            <label for="name">Имя: <input type="text" name="name" id="name" value="<?php htmlout($name);?>"></label>
+        </div>
+        <div>
+            <label for="email">Электронная почта: <input type="text" name="email" id="email" value="<?php htmlout($email);?>"></label>
+        </div>
+        <div>
+            <input type="hidden" name="id" value="<?php htmlout($id);?>">
+            <input type="submit" value="<?php htmlout($button);?>">
+        </div>
+    </form>
+</body>
+</html>
