b860163e5aa0aee6a81071f8a242ffc8ed0f7140
RuslanKozin <ruslan3723@mail.ru>
2016-04-06 16:47:44 +0300

    Добавил код редактирования категорий

--- admin/categories/index.php
+++ admin/categories/index.php
@@ -30,6 +30,53 @@ if (isset($_GET['addform'])) {
 }
 /* ........................................................................ */
 
+/* ...................... Редактирование категории ........................ */
+if (isset($_POST['action']) and $_POST['action'] == 'Редактировать') {
+    include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
+
+    try {
+        $sql = 'SELECT id, name FROM category WHERE id = :id';
+        $s = $pdo->prepare($sql);
+        $s->bindValue(':id', $_POST['id']);
+        $s->execute();
+    }
+    catch (PDOException $e) {
+        $error = 'Ошибка при извлечении информации о категории.' . $e->getMessage();
+        include 'error.html.php';
+        exit();
+    }
+
+    $row = $s->fetch();
+
+    $pageTitle = 'Редактировать категорию';
+    $action = 'editform';
+    $name = $row['name'];
+    $id = $row['id'];
+    $button = 'Обновить категорию';
+
+    include 'form.html.php';
+    exit();
+}
+if (isset($_GET['editform'])) {
+    include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
+
+    try {
+        $sql = 'UPDATE category SET name = :name WHERE id = :id';
+        $s = $pdo->prepare($sql);
+        $s->bindValue(':id', $_POST['id']);
+        $s->bindValue(':name', $_POST['name']);
+        $s->execute();
+    }
+    catch (PDOException $e) {
+        $error = 'Ошибка при обновлении категории.' . $e->getMessage();
+        include 'error.html.php';
+        exit();
+    }
+    header('Location: .');
+    exit();
+}
+/* ........................................................................ */
+
 //Удаляем все записи, связывающие шутки с этой категорией.
 try {
     $sql = 'DELETE FROM jokecategory WHERE categoryid = :id';
