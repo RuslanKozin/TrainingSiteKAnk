31a291214585edc8cb5a8b8e147041241372cc41
RuslanKozin <ruslan3723@mail.ru>
2016-04-06 15:47:02 +0300

    Добавил код удаления категории и связанных с ней записей

--- admin/categories/index.php
+++ admin/categories/index.php
@@ -1,1 +1,25 @@
 <?php
+//Удаляем все записи, связывающие шутки с этой категорией.
+try {
+    $sql = 'DELETE FROM jokecategory WHERE categoryid = :id';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':id', $_POST['id']);
+    $s->execute();
+}
+catch (PDOException $e) {
+    $error = 'Ошибка при удалении шуток из категории.' . $e->getMessage();
+    include 'error.html.php';
+    exit();
+}
+//Удаляем категорию.
+try {
+    $sql = 'DELETE FROM category WHERE id = :id';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':id', $_POST['id']);
+    $s->execute();
+}
+catch (PDOException $e) {
+    $error = 'Ошибка при удалении категории.' . $e->getMessage();
+    include 'error.html.php';
+    exit();
+}
