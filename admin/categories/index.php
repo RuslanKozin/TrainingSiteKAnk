377ffaae6b848e5419636331606954c294f4996d
RuslanKozin <ruslan3723@mail.ru>
2016-04-06 16:23:42 +0300

    Добавил код добавления категории

--- admin/categories/index.php
+++ admin/categories/index.php
@@ -1,4 +1,35 @@
-<?php
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/magicquotes.inc.php';
+
+/* ........................ Добавление категории ......................... */
+if (isset($_GET['add'])) {
+    $pageTitle = 'Новая категория';
+    $action = 'addform';
+    $name = '';
+    $id = '';
+    $button = 'Добавить категорию';
+
+    include 'form.html.php';
+    exit();
+}
+if (isset($_GET['addform'])) {
+    include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
+
+    try {
+        $sql = 'INSERT INTO category SET name = :name';
+        $s = $pdo->prepare($sql);
+        $s->bindValue(':name', $_POST['name']);
+        $s->execute();
+    }
+    catch (PDOException $e) {
+        $error = 'Ошибка при добавлении категории.' . $e->getMessage();
+        include 'error.html.php';
+        exit();
+    }
+    header('Location: .');
+    exit();
+}
+/* ........................................................................ */
+
 //Удаляем все записи, связывающие шутки с этой категорией.
 try {
     $sql = 'DELETE FROM jokecategory WHERE categoryid = :id';
