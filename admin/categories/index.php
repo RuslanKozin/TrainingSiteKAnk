59a992749edbcdcf95d1ca14b1f17baf0c9c3f09
RuslanKozin <ruslan3723@mail.ru>
2016-04-07 14:49:48 +0300

    Добавил комментарии для редактирования и удаления категорий

--- admin/categories/index.php
+++ admin/categories/index.php
@@ -65,17 +65,20 @@ if (isset($_GET['editform'])) {
 
     try {
         $sql = 'UPDATE category SET name = :name WHERE id = :id';
-        $s = $pdo->prepare($sql);
-        $s->bindValue(':id', $_POST['id']);
-        $s->bindValue(':name', $_POST['name']);
-        $s->execute();
+        $s = $pdo->prepare($sql);  /*Метод prepare направлляет MySQL серверу псевдопеременную находящуюся в $sql
+                    для подготовки выполнения команд, после возвращает объект PDOStatement и сохраняет в $s*/
+        $s->bindValue(':id', $_POST['id']);  /*С помощью метода bindValue заменяем псевдопеременную :id на $_POST['id']*/
+        $s->bindValue(':name', $_POST['name']);  /*С помощью метода bindValue заменяем псевдопеременную :name на $_POST['name']*/
+        $s->execute();  /*Метод execute говорит серверу MySQL выполнить запрос с предоставленными ему значениями*/
     }
     catch (PDOException $e) {
         $error = 'Ошибка при обновлении категории.' . $e->getMessage();
         include 'error.html.php';
         exit();
     }
-    header('Location: .');
+    header('Location: .');  /*отсылаем заголовок Location, чтобы объявить о перенаправлении.
+                Точка . обозначает текущий документ/директорию т.е. нужно перезагрузить текущую директорию
+                после добавления шутки в базу данных*/
     exit();
 }
 /* ........................................................................ */
@@ -87,9 +90,10 @@ if (isset($_POST['action']) and $_POST['action'] == 'Удалить') {
     //Удаляем все записи, связывающие шутки с этой категорией.
     try {
         $sql = 'DELETE FROM jokecategory WHERE categoryid = :id';
-        $s = $pdo->prepare($sql);
-        $s->bindValue(':id', $_POST['id']);
-        $s->execute();
+        $s = $pdo->prepare($sql);  /*Метод prepare направлляет MySQL серверу псевдопеременную находящуюся в $sql
+                    для подготовки выполнения команд, после возвращает объект PDOStatement и сохраняет в $s*/
+        $s->bindValue(':id', $_POST['id']);  /*С помощью метода bindValue заменяем псевдопеременную :id на $_POST['id']*/
+        $s->execute();  /*Метод execute говорит серверу MySQL выполнить запрос с предоставленными ему значениями*/
     }
     catch (PDOException $e) {
         $error = 'Ошибка при удалении шуток из категории.' . $e->getMessage();
@@ -108,7 +112,9 @@ if (isset($_POST['action']) and $_POST['action'] == 'Удалить') {
         include 'error.html.php';
         exit();
     }
-    header('Location: .');
+    header('Location: .');  /*отсылаем заголовок Location, чтобы объявить о перенаправлении.
+                Точка . обозначает текущий документ/директорию т.е. нужно перезагрузить текущую директорию
+                после добавления шутки в базу данных*/
     exit();
 }
 /* ............................................................................. */
