9caf98327f3aa5ffe4dd2475957d4ace3851e70c
RuslanKozin <ruslan3723@mail.ru>
2016-04-07 15:24:07 +0300

    Добавил код вывода формы поиска

--- admin/jokes/index.php
+++ admin/jokes/index.php
@@ -1,1 +1,31 @@
 <?php
+
+/* ........................ Выводим форму поиска ....................... */
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
+
+try {
+    $result = $pdo->query('SELECT id, name FROM author');
+}
+catch (PDOException $e) {
+    $error = 'Ошибка при извлечении записей об авторах!';
+    include 'error.html.php';
+    exit();
+}
+foreach ($result as $row) {
+    $authors[] = array('id' => $row['id'], 'name' => $row['name']);
+}
+
+try {
+    $result = $pdo->query('SELECT id, name FROM category');
+}
+catch (PDOException $e) {
+    $error = 'Ошибка при извлечениии категорий из базы данных!';
+    include 'error.html.php';
+    exit();
+}
+
+foreach ($result as $row) {
+    $categories[] = array('id' => $row['id'], 'name' => $row['name']);
+}
+include 'searchform.html.php';
+/*........................................................................*/
