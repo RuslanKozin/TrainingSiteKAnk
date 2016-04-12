16538db5a5d90eacc6c6c0ed2ee56382b091d2f0
RuslanKozin <ruslan3723@mail.ru>
2016-04-12 23:34:13 +0300

    Добавил код для поиска

--- admin/jokes/index.php
+++ admin/jokes/index.php
@@ -1,5 +1,45 @@
 <?php
 
+/* Запрос при котором не выбран не один критерий */
+if (isset($_GET['action']) and $_GET['action'] == 'search') {
+    include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
+
+    //Базовое выражение SELECT.
+    $select = 'SELECT id, joketext';
+    $from = ' FROM joke';
+    $where = ' WHERE TRUE';
+}
+
+$placeholders = array();
+if ($_GET['author'] != '') {  // Автор выбран.
+    $where .= " AND authorid = :authorid";
+    $placeholders[':authorid'] = $_GET['author'];
+}
+if ($_GET['category'] != '') {  //Категория выбрана.
+    $from .= ' INNER JOIN jokecategory ON id = jokeid';
+    $where .= " AND categoryid = :categoryid";
+    $placeholders[':categoryid'] = $_GET['category'];
+}
+if ($_GET['text'] != '') {  //Была указана какая-то искомая строка.
+    $where .= " AND joketext LIKE :joketext";
+    $placeholders[':joketext'] = '%' . $_GET['text'] . '%';
+}
+try {
+    $sql = $select . $from . $where;
+    $s = $pdo->prepare($sql);
+    $s->execute($placeholders);
+}
+catch (PDOException $e) {
+    $error = 'Ошибка при извлечении шуток.';
+    include 'error.html.php';
+    exit();
+}
+foreach ($s as $row) {
+    $jokes[] = array('id' => $row['id'], 'text' => $row['joketext']);
+}
+include 'jokes.html.php';
+exit();
+
 /* ........................ Выводим форму поиска ....................... */
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
 
