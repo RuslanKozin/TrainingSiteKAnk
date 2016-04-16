62b140356efa3bd460e90945de7d95a8b08a60af
RuslanKozin <ruslan3723@mail.ru>
2016-04-16 19:13:49 +0300

    Исправил комментарии

--- admin/jokes/searchform.html.php
+++ admin/jokes/searchform.html.php
@@ -8,17 +8,17 @@
 <body>
     <h1>Управление шутками</h1>
     <p><a href="?add">Добавить новую шутку</a></p>
-    <form action="" method="get">  /* Метод GET в атрибуте method дает возможность добавлять результаты поиска в закладки,
-                            поскольку значения формы в этом случае будут представлять часть адреса URL в строке запроса.*/
+    <form action="" method="get">  <!--Метод GET в атрибуте method дает возможность добавлять результаты поиска в закладки,
+                            поскольку значения формы в этом случае будут представлять часть адреса URL в строке запроса-->
         <p>Вывести шутки, которые удовлетворяют следующим критериям:</p>
         <div>
             <label for="author">По автору:</label>
             <select name="author" id="author">
-                <option value="">Любой автор</option>  /*Раскрывающийся список начинается элементом option с пустым
-                    атрибутом value, что позволяет исключить поле из критериев поиска.*/
+                <option value="">Любой автор</option>  <!--Раскрывающийся список начинается элементом option с пустым
+                    атрибутом value, что позволяет исключить поле из критериев поиска-->
                 <?php foreach ($authors as $author): ?>
-                    <option value="<?php htmlout($author['id']);?>">  /*Идентификатор автора*/
-                        <?php htmlout($author['name']);?>   /* Имя автора */
+                    <option value="<?php htmlout($author['id']);?>">  <!--Идентификатор автора-->
+                        <?php htmlout($author['name']);?>   <!-- Имя автора -->
                     </option>
                 <?php endforeach; ?>
             </select>
@@ -26,11 +26,11 @@
         <div>
             <label for="category">По категориям:</label>
             <select name="category" id="category">
-                <option value="">Любая категория</option>  /*Раскрывающийся список начинается элементом option с пустым
-                атрибутом value, что позволяет исключить поле из критериев поиска.*/
+                <option value="">Любая категория</option>  <!--Раскрывающийся список начинается элементом option с пустым
+                атрибутом value, что позволяет исключить поле из критериев поиска.-->
                 <?php foreach ($categories as $category): ?>
-                    <option value="<?php htmlout($category['id']);?>">  /*Идентификатор категории*/
-                        <?php htmlout($category['name']);?>     /* Название категории */
+                    <option value="<?php htmlout($category['id']);?>">  <!--Идентификатор категории-->
+                        <?php htmlout($category['name']);?>     <!-- Название категории -->
                     </option>
                 <?php endforeach;?>
             </select>
