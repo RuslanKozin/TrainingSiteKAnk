d7a9888fdba8048f49428950d11b80aa78d1a13b
RuslanKozin <ruslan3723@mail.ru>
2016-04-17 00:32:57 +0300

    Добавил комментарии

--- admin/jokes/form.html.php
+++ admin/jokes/form.html.php
@@ -14,10 +14,14 @@
 <body>
     <hl><?php htmlout($pageTitle); ?></hl>
     <form action="?<?php htmlout($action); ?>" method="post">
+        <!-- ................... Область ввода текста шутки ..................... -->
         <div>
             <label for="text">Введите сюда свою шутку:</label>
             <textarea id="text" name="text" rows="3" cols="40"><?php htmlout($text); ?></textarea>
         </div>
+        <!-- .................................................................... -->
+
+        <!-- ................... Область выбора имя автора ...................... -->
         <div>
             <label for="author">Автор:</label>
             <select name="author" id="author">
@@ -25,28 +29,34 @@
                 <?php foreach ($authors as $author): ?>
                     <option value="<?php htmlout($author['id']); ?>"<?php
                         if ($author['id'] == $authorid) {
-                            echo ' selected';
+                            echo ' selected';  /*Добавляем атрибут selected в тег option, если идентификатор
+                             соответствуюшего автора $author['id'] совпадает с полем $authorid редактируемой шутки.*/
                         }
                         ?>><?php htmlout ($author['name']);  ?></option>
                 <?php endforeach; ?>
             </select>
         </div>
+        <!-- .................................................................... -->
+
+        <!-- .................... Область выбора категории ...................... -->
         <fieldset>
             <legend>Категории:</legend>
             <?php foreach ($categories as $category): ?>
                 <div>
                     <label for="category<?php htmlout($category['id']); ?>">
-                        <input type="checkbox" name="categories[]"
+                        <input type="checkbox" name="categories[]"   /*Массив categories[] нужен для отправки
+                                                                        лемента формы как части массива*/
                             id="category<?php htmlout($category['id']); ?>"
-                            value="<?php htmlout($category['id']); ?>"<?php
-                                if ($category['selected']) {
-                                    echo ' checked';
+                            value="<?php htmlout($category['id']); ?>"<?php  /*Задаем id соответствующей категории*/
+                                if ($category['selected']) {  //Если шутка из соответствующей категории, то
+                                    echo ' checked';          //укажем атрибут selected
                                 }
                             ?>><?php htmlout($category['name']); ?>
                     </label>
                 </div>
             <?php endforeach; ?>
         </fieldset>
+        <!--.......................................................................-->
         <div>
             <input type="hidden" name="id" value="<?php htmlout($id); ?>">
             <input type="submit" value="<?php htmlout($button); ?>">
