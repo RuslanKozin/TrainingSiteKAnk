6d7166db0b1aeb00d007d69476a6cb4f99188f73
RuslanKozin <ruslan3723@mail.ru>
2016-04-19 15:56:54 +0300

    Добавил код

--- chapter8/preg_match1/index.php
+++ chapter8/preg_match1/index.php
@@ -1,1 +1,10 @@
 <?php
+$text = 'PHP рулит!';
+
+if (preg_match('/PHP/', $text)) {
+    $output = '$text содержит строку &ldquo;PHP&rdquo;.';
+}
+else {
+    $output = '$text не содержит строку &ldquo;PHP&rdquo;.';
+}
+include 'output.html.php';
