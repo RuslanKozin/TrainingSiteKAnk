fa6cdca7e3c82dd6d9eee0af4bc586635f1fb655
RuslanKozin <ruslan3723@mail.ru>
2016-05-12 19:19:17 +0300

    Добавил код

--- cookiecounter/welcome.html.php
+++ cookiecounter/welcome.html.php
@@ -1,1 +1,21 @@
-<?php
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/helpers.inc.php'; ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Счетчик cookie</title>
+</head>
+<body>
+    <p>
+        <?php
+            if ($visits > 1) {
+                echo "Номер данного поседения: $visits.";
+            }
+            else {
+                //Первое посещение
+                echo 'Добро пожаловать на мой веб-сайт! Кликните здесь, чтобы узнать больше!';
+            }
+        ?>
+    </p>
+</body>
+</html>
