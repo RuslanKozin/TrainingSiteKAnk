e2a2a79242c3da9381ffbc9f5e70f31a64bb246f
RuslanKozin <ruslan3723@mail.ru>
2016-05-15 16:10:08 +0300

    Добавил код и комментарии

--- admin/logout.inc.html.php
+++ admin/logout.inc.html.php
@@ -1,1 +1,9 @@
-<?php
+<form action="" method="post">
+    <div>       /*Используется скрытое поле с именем action, которое сообщает о намерениях пользователя.
+                Поле goto указывает на то, куда необходимо перенаправить пользователя, который только что вышел
+                из системы.*/
+        <input type="hidden" name="action" value="logout">
+        <input type="hidden" name=ngoto" value="/admin/">
+        <input type="submit" value="Выйти">
+    </div>
+</form>
