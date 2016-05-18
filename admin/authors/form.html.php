3cc759821016d802d6caf47d58511090ecbe35af
RuslanKozin <ruslan3723@mail.ru>
2016-05-18 14:58:05 +0300

    Добавил код назначение ролей

--- admin/authors/form.html.php
+++ admin/authors/form.html.php
@@ -14,6 +14,29 @@
         <div>
             <label for="email">Электронная почта: <input type="text" name="email" id="email" value="<?php htmlout($email);?>"></label>
         </div>
+        <!-- .......................................... Назначение ролей ........................................... -->
+        <div>
+            <label for="password">Задать пароль: <input type="password" name="password" id="password"></label>
+        </div>
+        <fieldset>
+            <legend>Roles:</legend>
+            <?php for ($i = 0; $i < count($roles); $i++): ?>
+                <div>
+                    <label for="role<?php echo $i; ?>">
+                        <input type="checkbox" name="roles[]" id="role<?php echo $i; ?>"
+                               value="<?php htmlout($roles[$i]['id']); ?>"
+                        <?php
+                            if ($roles[$i]['selected']) {
+                                echo 'checked';
+                            }
+                        ?>
+                        <?php htmlout($roles[$i]['id']); ?>>
+                    </label>
+                    <?php htmlout($roles[$i]['description']); ?>
+                </div>
+            <?php endfor; ?>
+        </fieldset>
+        <!-- ....................................................................................................... -->
         <div>
             <input type="hidden" name="id" value="<?php htmlout($id);?>">
             <input type="submit" value="<?php htmlout($button);?>">
