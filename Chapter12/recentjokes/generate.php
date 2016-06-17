c9c252d8e3cac4e443062085a14940be89601069
RuslanKozin <ruslan3723@mail.ru>
2016-06-17 19:49:07 +0300

    Добавил код

--- Chapter12/recentjokes/generate.php
+++ Chapter12/recentjokes/generate.php
@@ -1,1 +1,6 @@
 <?php
+$srcurl = 'http://localhost/recentjokes/controller.php';
+$tempfilename = $_SERVER['DOCUMENT_ROOT'] .
+    '/recentjokes/tempindex.html';
+$targetfilename = $_SERVER['DOCUMENT_ROOT'] .
+    '/recentjokes/index.html';
