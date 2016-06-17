3224ea72d9aa39a4fe8d41e42f8597469cd697f9
RuslanKozin <ruslan3723@mail.ru>
2016-06-17 19:37:47 +0300

    Добавил код

--- Chapter12/recentjokes/jokes.html.php
+++ Chapter12/recentjokes/jokes.html.php
@@ -1,1 +1,15 @@
-<?php
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/helpers.inc.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Последние шутки</title>
+    <link rel="canonical" href="/recentjokes/">
+</head>
+<body>
+    <р>Вот самые свежие шутки в базе данных:</р>
+    <?php foreach ($jokes  as  $joke): ?>
+        <?php markdownout($joke['text']); ?>
+    <?php endforeach; ?>
+</body>
+</html>
