d77ee42b13f25da59957b0ae07629e4283f2f402
RuslanKozin <ruslan3723@mail.ru>
2016-06-17 19:33:28 +0300

    Добавил код

--- Chapter12/recentjokes/controller.php
+++ Chapter12/recentjokes/controller.php
@@ -1,1 +1,14 @@
-<?php
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
+try {
+    $sql = 'SELECT id, joketext FROM joke ORDER BY jokedate DESC LIMIT 3';
+    $result =  $pdo->query($sql);
+}
+catch (PDOException $e) {
+    $error = 'Ошибка при получении шуток.';
+    include $_SERVER['DOCUMENT_ROOT'] . '/includes/error.html.php';
+    exit();
+}
+foreach ($result as $row) {
+    $jokes[] = array('text' => $row['joketext']);
+}
+include 'jokes.html.php';
